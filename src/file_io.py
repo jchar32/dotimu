4560fd6ab5193886f560034824495df8251c85ac
Jesse Charlton <57236497+jchar32@users.noreply.github.com>
2023-10-31 16:41:35 -0700

    refoctoring and clean up

--- src/file_io.py
+++ src/file_io.py
@@ -2,19 +2,33 @@ import pandas as pd
 import os
 import time
 from dot import Dot
-from typing import List
+from typing import List, Dict
 
 def read_from_csv(path):
     df = pd.read_csv(path, header=0)
     return df.drop(index=0).reset_index(drop=True)
 
 
-def load_dot_file(filenames, datapath):
+def load_dot_file(filenames: str, datapath: str) -> Dict[str, Dot]:
+    """
+    Load data from .csv files into a dictionary.
+
+    This function reads data from a list of .csv files, each file corresponding to single dot sensor file.
+    The data from each file is stored in a dictionary where the keys are the dot sensor IDs and the values are Dot objects.
+
+    Args:
+        filenames (List[str]): A list of filenames to read data from.
+        datapath (str): The path to the directory where the .csv files are stored.
+
+    Returns:
+        dotdata (Dict[str, Dot]): A dictionary where the keys are sensor IDs and the values are Dot objects containing.
+    """
     data_dict = {}
     dotdata = {}
     filenum = 0
     last_id = filenames[0].split("_")[0]
     dotid = [last_id]
+
     for i, file in enumerate(filenames):
         temp = read_from_csv(os.path.join(datapath, file))
         id = file.split("_")[0]  # get the sensor ID
@@ -34,6 +48,11 @@ def load_dot_file(filenames, datapath):
 
 
 def __dot_data_indices():
+    """Specify each data channel to export from the DOT sensor.
+
+    Returns:
+        XsIntArray: A list of data channels to export from the DOT sensor. Type is native to the C++ API used behind the scenes.
+    """
     import movelladot_pc_sdk as sdk
     exportData = sdk.XsIntArray()  # type: ignore
     exportData.push_back(sdk.RecordingData_Timestamp)  # type: ignore
@@ -57,8 +76,25 @@ def dir_exists_or_makeit(path: str) -> None:
         os.makedirs(path)
 
 def export_dot_data(dots2export: List[int] | str = "all", path: str = "data/") -> None:
+    """
+    Exports data from USB connected Dot devices to CSV files.
+
+    This function initializes a connection to all detected Dot devices, selects the data channels to export,
+    and exports the data to CSV files.
+    The files are named in the format "dot_{device_id}_{fileIndex}.csv"
+    and are saved in a /data/ folder within the current working dir.
+
+    Args:
+        dots2export (Union[List[int], str], optional): A list of indices for which Dots to export or "all" to export all.
+            Defaults to "all".
+        path (str, optional): The path to the directory to save the CSV files to. Defaults to "../data/".
+    Returns:
+        None
+    """
     from xdpchandler import XdpcHandler
 
+    if path != "data/":
+        raise NotImplementedError("Only the default path is currently supported.")
     dir_exists_or_makeit(path)
 
     dotio = XdpcHandler()
