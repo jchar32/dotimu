bf010f68ab60123b87c330bf7880be4ede770a78
Jesse Charlton <57236497+jchar32@users.noreply.github.com>
2023-11-08 14:21:03 -0800

    condensing and a few optimizations of get_path

--- src/ui.py
+++ src/ui.py
@@ -16,39 +16,35 @@ def get_path(wildcard: str = ""):
     app = wx.App(redirect=False)
 
     style = wx.FD_OPEN | wx.FD_FILE_MUST_EXIST | wx.STAY_ON_TOP | wx.FD_MULTIPLE | wx.FD_NO_FOLLOW
-    dialog = wx.FileDialog(None, "Select file(s) you wihs to load", defaultDir=current_dir, wildcard=wildcard, style=style)
+    dialog = wx.FileDialog(None, "Select file(s) you want to load", defaultDir=current_dir, wildcard=wildcard, style=style)
 
     direc = ""
     file = ""
 
-    if dialog.ShowModal() == wx.ID_CANCEL:
-        direc = ""
-        file = ""
+    with wx.FileDialog(None, "Select file(s) you want to load", defaultDir=current_dir, wildcard=wildcard, style=style) as dialog:
 
-    if dialog.ShowModal() == wx.ID_OK:
-        paths = dialog.GetPaths()
-
-        if len(paths) == 1:
-            direc = os.path.dirname(paths)
-            file = os.path.basename(paths)
-        elif len(paths) > 1:
-            direc = []
-            file = []
-            for i, p in enumerate(paths):
-                direc.append(os.path.dirname(p))
-                file.append(os.path.basename(p))
-        else:
-            print("No file selected")
+        if dialog.ShowModal() == wx.ID_CANCEL:
             direc = ""
             file = ""
+            wx.LogError("No file selected")
+            return direc, file
 
-    dialog.Destroy()
+        paths = dialog.GetPaths()
+        direc = os.path.dirname(paths[0])
+        if len(paths) == 1:
+            file = os.path.basename(paths)
+        elif len(paths) > 1:
+            file = []
+            for p in paths:
+                file.append(os.path.basename(p))
+        else:
+            wx.LogError("No file selected")
+
     app.Destroy()
     return direc, file
 
 if __name__ == "__main__":
     d, p = get_path()
-    for i in d:
-        print(i)
+    print(f"\n Directory Selected: {d} --> \n")
     for j in p:
         print(j)
