f00851c6ac311c7165687496ef5587fd44663a31
Jesse Charlton <57236497+jchar32@users.noreply.github.com>
2023-12-13 16:38:11 -0800

    refactored sensor sync functions

--- src/calibration.py
+++ src/calibration.py
@@ -296,30 +296,33 @@ def __flip_left_ml_axis(evecs, side):
 
 
 def find_common_start_time(data: dict) -> dict:
-    trial_nums = data[list(data.keys())[0]].data.keys()
+    num_files = len(data[list(data.keys())[0]])
 
     # collect the first time stamp for each trial on each sensor
-    test = np.full((len(data.keys()), len(trial_nums)), np.nan)
-    for i, trial in enumerate(trial_nums):
-        for j, s in enumerate(data.keys()):
-            test[j, i] = data[s].data[trial].loc[0, "SampleTimeFine"]
+    test = np.full((len(data.keys()), num_files), np.nan)
+    for j, s in enumerate(data.keys()):
+        for i, trial in enumerate(data[s]):
+            test[j, i] = trial.loc[0, "SampleTimeFine"]
 
     latest_starttime = np.max(test, axis=0)  # the highest common timestamp
 
     # get the row index for the common time stamps for every sensor across each trial.
     index_to_trim_start = {s: [] for s in data.keys()}
-    for t in range(len(trial_nums)):
-        for s in data.keys():
+    for s in data.keys():
+        for i, trial in enumerate(data[s]):  # range(num_files):
             index_to_trim_start[s].append(
-                np.where(data[s].data[t].loc[:, "SampleTimeFine"] == latest_starttime[t])[0][0]
+                np.where(trial.loc[:, "SampleTimeFine"] == latest_starttime[i])[0][0]
             )
+
     return index_to_trim_start
 
 
 def sync_multi_dot(data, syncidx):
     syncd_data = copy.deepcopy(data)
-    for s in syncd_data.keys():
-        for t in syncd_data[s].data.keys():
-            syncd_data[s].data[t] = syncd_data[s].data[t].iloc[syncidx[s][t] :, :]
-            syncd_data[s].data[t].reset_index(inplace=True)
+
+    for s in syncd_data:
+        for i, trial in enumerate(syncd_data[s]):
+            trimmed_data = trial.iloc[syncidx[s][i] :, :]
+            trimmed_data.reset_index(inplace=True)
+            syncd_data[s][i] = trimmed_data
     return syncd_data
